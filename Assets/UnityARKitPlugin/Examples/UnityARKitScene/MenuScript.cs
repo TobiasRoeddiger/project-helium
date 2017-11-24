8d19bbc94747af480335091d2bba36ec1e5a4884
MatthiasHeg <matthiasheger@aol.com>
2017-11-24 21:21:49 +0100

    Updated starting sequence

--- Assets/UnityARKitPlugin/Examples/UnityARKitScene/MenuScript.cs
+++ Assets/UnityARKitPlugin/Examples/UnityARKitScene/MenuScript.cs
@@ -6,11 +6,17 @@ using UnityEngine.UI;
 public class MenuScript : MonoBehaviour {
 
 	private GameObject detectingObject;
+	private GameObject steeringWheel;
 	bool running = false;
+	public float timer;
 
 	public void Start() {
 		detectingObject = GameObject.Find ("DetectingObjectText");
 		detectingObject.SetActive (false);
+
+		steeringWheel = GameObject.Find ("SteeringWheel");
+		steeringWheel.SetActive (false);
+
 	}
 
 	public void StartButtonClicked(string name) {
@@ -20,15 +26,28 @@ public class MenuScript : MonoBehaviour {
 		Destroy (startButton);
 	}
 
-	private float timeLeft = 5;
+	private float timeLeft = 3;
 	public void Update()
 	{
+
 		if (running) {
+			//Blinking function:
+			timer += Time.deltaTime;
+			if (timer >= 0.5) {
+				detectingObject.SetActive (false);
+			}
+			if (timer > 1) {
+				detectingObject.SetActive (true);
+				timer = 0;
+			}
+			//END Blinking function
+
 			timeLeft -= (float)Time.deltaTime;
 			if (timeLeft < 0.0f)
 			{
 				running = false;
-				detectingObject.SetActive (false);
+				//detectingObject.SetActive (false);
+				steeringWheel.SetActive (true);
 			}
 		}
 	}
