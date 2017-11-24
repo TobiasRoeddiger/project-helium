a209dd64a874992061af0c33b273a6edb3b48709
Tobias Röddiger <tobiasroddiger@Tobiass-MacBook-Pro.local>
2017-11-24 18:13:50 +0100

    stuff

--- Assets/UnityARKitPlugin/Examples/UnityARKitScene/MenuScript.cs
+++ Assets/UnityARKitPlugin/Examples/UnityARKitScene/MenuScript.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class MenuScript : MonoBehaviour {
 
 	private GameObject detectingObject;
+	bool running = false;
 
 	public void Start() {
 		detectingObject = GameObject.Find ("DetectingObjectText");
@@ -14,7 +15,21 @@ public class MenuScript : MonoBehaviour {
 
 	public void StartButtonClicked(string name) {
 		detectingObject.SetActive (true);
+		running = true;
 		var startButton = GameObject.Find ("StartButton");
 		Destroy (startButton);
+	}
+
+	private float timeLeft = 5;
+	public void Update()
+	{
+		if (running) {
+			timeLeft -= (float)Time.deltaTime;
+			if (timeLeft < 0.0f)
+			{
+				running = false;
+				detectingObject.SetActive (false);
+			}
+		}
 	}
 }
