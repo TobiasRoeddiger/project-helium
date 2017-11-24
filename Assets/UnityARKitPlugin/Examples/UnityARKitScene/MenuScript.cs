43f616de286a56aeb21019f514a7e44683541cab
Tobias Röddiger <tobiasroddiger@Tobiass-MacBook-Pro.local>
2017-11-24 17:48:02 +0100

    added files

---
+++ Assets/UnityARKitPlugin/Examples/UnityARKitScene/MenuScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour {
+
+	public void Start() {
+		
+	}
+
+	public void StartButtonClicked(string name) {
+		var startButton = GameObject.Find ("StartButton");
+		Destroy (startButton);
+	}
+}
