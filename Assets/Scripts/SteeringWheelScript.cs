3c8353ba1249906e5a8d40d18dd3952b61d1b26c
Dominik Doerner <nik_doerner@web.de>
2017-11-24 22:26:17 +0100

    Added separate scene for displaying the steering wheel + HUD

---
+++ Assets/Scripts/SteeringWheelScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SteeringWheelScript : MonoBehaviour {
+
+	private GameObject toggleButton;
+	private GameObject steeringWheel;
+	private GameObject Hud;
+	public float timer;
+
+	private bool _toggleState;
+	public bool toggleState {
+		get {
+			return _toggleState;
+		}
+		set {
+			_toggleState = value;
+			Hud.SetActive (value);
+		}
+	}
+
+	public void Start() {
+		Hud = GameObject.Find ("Hud");
+		toggleState = false;
+
+		steeringWheel = GameObject.Find ("SteeringWheel");
+		steeringWheel.SetActive (false);
+	}
+
+	public void HudToggleButtonClicked() {
+		toggleState = !toggleState;
+	}
+
+	public void Update()
+	{
+		if (timer >= 0) {
+			timer += Time.deltaTime;
+			if (timer > 3) {
+				timer = -1;
+				HudToggleButtonClicked ();
+			}
+		}
+	}
+}
