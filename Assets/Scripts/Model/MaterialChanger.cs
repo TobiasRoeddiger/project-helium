226643c68e5d6bac6840919cda4f69c118a472c0
Dominik Doerner <nik_doerner@web.de>
2017-11-25 01:55:44 +0100

    Material change fix

--- Assets/Scripts/Model/MaterialChanger.cs
+++ Assets/Scripts/Model/MaterialChanger.cs
@@ -5,10 +5,12 @@ public static class MaterialChanger
 {
 	public static void ChangeMaterial(this GameObject gObject, string texture, MaterialSample sample = null) {
 		Material newMat = Resources.Load(texture, typeof(Material)) as Material;
-		gObject.GetComponent<Renderer>().material = newMat;
+		foreach (var renderer in gObject.GetComponentsInChildren<Renderer>())
+			renderer.material = newMat;
 
 		if (sample != null) {
 			// TODO: assign physical properties
+
 		}
 	}
 }
